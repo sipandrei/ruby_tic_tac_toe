4472f0edaebbcbf15703c91db756275e4e8e2d21
sipandrei <pontos.serban@gmail.com>
2021-04-16 15:08:32 +0300

    Added functions for finishing the game, it works!

--- tic_tac_toe.rb
+++ tic_tac_toe.rb
@@ -2,7 +2,7 @@
 
 # Jucatorul jocului
 class Player
-  attr_reader :name, :letter, :position, :won
+  attr_reader :name, :letter, :position
 
   @@player_letters = []
   def initialize(name, letter)
@@ -14,7 +14,6 @@ class Player
     end
     @letter = letter[0]
     @@player_letters << @letter
-    @won = false
   end
 
   def next_move
@@ -48,15 +47,49 @@ class Board
   end
 
   def update_board(player)
-    index = player.position
-
-    while  @board[index / 3][index % 3] != '.'
+    while  @board[player.position / 3][player.position % 3] != '.'
       puts 'You chose a taken position'
       player.next_move
-      index = player.position
     end
-    @board[index / 3][index % 3] = player.letter
+    @board[player.position / 3][player.position % 3] = player.letter
     display_board
+  end
+
+  def end_game(player)
+    if won_diag?(player.letter) || won_col?(player) || won_row?(player)
+      @game_over = true
+      puts "#{player.name} won the game!"
+    end
+  end
+
+  private
+
+  def won_diag?(character)
+    num = 0
+    @board.each_with_index do |row, index|
+      num += 1 if character == row[index]
+    end
+    return true if num == 3
+
+    num = 0
+    @board.each_with_index do |row, index|
+      num += 1 if character == row[2 - index]
+    end
+    true if num == 3
+  end
+
+  def won_row?(player)
+    row = @board[player.position / 3]
+    true if row.uniq.length == 1 && row.uniq[0] == player.letter
+  end
+
+  def won_col?(player)
+    col = player.position % 3
+    won = true
+    @board.each do |row|
+      won = false if row[col] != player.letter
+    end
+    won
   end
 end
 
@@ -72,7 +105,7 @@ puts "What letter do you want to see on the board, #{name}?"
 letter = gets.chomp
 player2 = Player.new(name, letter)
 
-puts "\n-------The game is starting------\n"
+puts "\n---------Game Started--------\n"
 
 game_board = Board.new
 game_board.display_board
@@ -81,9 +114,11 @@ print "\n"
 until game_board.game_over == true
   player1.next_move
   game_board.update_board(player1)
-  # Check if he won
+  game_board.end_game(player1)
+
+  break if game_board.game_over
 
   player2.next_move
   game_board.update_board(player2)
-  # Check if he won
+  game_board.end_game(player2)
 end
